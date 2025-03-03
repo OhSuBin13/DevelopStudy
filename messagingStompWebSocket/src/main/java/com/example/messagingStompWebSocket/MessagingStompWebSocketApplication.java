2bfbecb94c3a2a56e041e8d435bf85ba81d0edf6
OhSuBin13 <dhtnqls0103@naver.com>
2025-03-03 16:32:53 +0900

    developed a STOMP-based messaging service with Spring.

---
+++ messagingStompWebSocket/src/main/java/com/example/messagingStompWebSocket/MessagingStompWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.messagingStompWebSocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingStompWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingStompWebSocketApplication.class, args);
+	}
+
+}
